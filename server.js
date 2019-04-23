059b962822628346866bb036f49b05ed5a39e365
RoryLetteney <rory.letteney@gmail.com>
2019-04-23 11:01:53 -0500

    Weather route built

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const express = require('express'),
   app = express(),
   PORT = process.env.PORT || 3000;
 
+// CREATE LOCATION ROUTE
 app.get('/location', (req, res) => {
+  // STORE THE USER'S QUERY-TURNED-LOCATION-OBJECT IN LOCATIONDATA
   const locationData = searchToLatLong(req.query.data);
+  // RETURN THE LOCATION OBJECT
   res.send(locationData);
 });
 
+// CREATE WEATHER ROUTE
+app.get('/weather', (req, res) => {
+  // STORE THE USER'S QUERY LOCATION 
+  const weatherData = getWeather();
+  res.send(weatherData);
+});
+
+// CREATE A NEW LOCATION OBJECT FOR THE USER'S QUERY
 const searchToLatLong = query => {
   const geoData = require('./data/geo.json');
   const location = new Location(query, geoData);
@@ -20,6 +31,23 @@ function Location(query, res) {
   this.formatted_query = res.results[0].formatted_address,
   this.latitude = res.results[0].geometry.location.lat,
   this.longitude = res.results[0].geometry.location.lng;
+}
+
+// RETURN ALL WEATHER RECORDS FOR THE USER'S LOCATION QUERY
+const getWeather = () => {
+  const darkskyData = require('./data/darksky.json');
+  const weatherSummaries = [];
+
+  darkskyData.daily.data.forEach(day => {
+    weatherSummaries.push(new Weather(day));
+  });
+
+  return weatherSummaries;
+};
+
+function Weather(day) {
+  this.forecast = day.summary,
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
 
 app.listen(PORT, () => console.log(`App is up and running on ${PORT}`));
